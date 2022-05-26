b2bff148d412769b9820ca8000605bb352020061
Jonathan Mondragón <32645185+Jonathan9519@users.noreply.github.com>
2022-05-26 18:30:49 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import React, { useState } from 'react'
 import './App.css';
-import { Input } from '@chakra-ui/react'
+import { Input, FormControl, Checkbox } from '@chakra-ui/react'
 
 function App() {
-  const [todoItems, setTodoItems] = useState([]);
   const [taskName, setTaskName] = useState("");
+  const [todoItems, setTodoItems] = useState([
+    { name: 'Buy milk', status: 'uncomplete' },
+    { name: 'Wash car', status: 'uncomplete' },
+    { name: 'Cut gras', status: 'complete' },
+  ]);
 
   const addNewList = () => {
+    try {
+      if (taskName === '') {
+        alert('')
+      }
+    } catch (error) {
 
+    }
   }
+
+  const changeItemProp = (prop, ix, value) => {
+    const clone = [...todoItems];
+    clone[ix] = { ...todoItems[ix], [prop]: value }
+    setTodoItems(clone);
+  }
+
+  const renderItems = todoItems.map((it, ix) => {
+    return <div style={{ width: '100%', padding: 5 }}>
+      <div
+        className='item-card'
+      >
+        <div class="ui checkbox">
+          <input 
+          type="checkbox"
+           name="example" 
+          />
+          <label>{it.name}</label>
+        </div>
+        <Checkbox
+          isChecked={it.status === 'complete'}
+          onChange={() => {
+            let status = it.status === 'complete' ? 'uncomplete' : 'complete';
+            changeItemProp('status', ix, status)
+          }}
+          colorScheme='green' defaultChecked>
+          {it.name}
+        </Checkbox>
+      </div>
+    </div>
+  })
 
   return (
     <div className="App">
@@ -32,19 +73,25 @@ function App() {
               Todo list
             </label>
             <div style={{
-              width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center', display: 'flex'
+              width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center'
             }}>
               {todoItems.length === 0
                 ? "No things to do"
-                : "hay"}
+                : <FormControl
+                  style={{ width: '100%', height: '100%' }}
+                >
+                  {/* <FormLabel htmlFor='country'>List</FormLabel> */}
+                  {renderItems}
+                </FormControl>}
+
             </div>
             <div className='row-container' >
               <Input
-                style={{ width: '90%', padding: 10, borderRadius:6, border:'1px solid lightgray' }}
+                style={{ width: '90%', padding: 10, borderRadius: 6, border: '1px solid lightgray' }}
                 placeholder='Basic usage'
                 size='md'
                 variant='filled'
-                onChange={() => setTaskName()}
+                onChange={(e) => setTaskName(e.target.value)}
               />
             </div>
           </div>
